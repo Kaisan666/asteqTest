6349be8017309a0ffb6f8274fcee2e550467f351
Kaisan <sasha41187@gmail.com>
2025-02-04 08:53:13 +0300

    фикс анимации + попап форма начат

---
+++ js/menuButton.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector(".menu-button")
+const menuForm = document.querySelector(".popup-form")
+menuButton.addEventListener("click", () =>{
+    menuForm.classList.add("active")
+})
